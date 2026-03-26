47e305ff667594ffe6374fcc5187369ce1c37e3b
Gabriel Branquinho <gabrielranulfo14@gmail.com>
2026-03-25 23:58:00 -0300

    atualização do run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,10 @@
 export RUN_LOG_TIMINGS=1
-export SCALE_FACTOR=1.0
+export SCALE_FACTOR=${SCALE_FACTOR:-1}
 
-#echo run with cached IO
-make tables
-#make  run-modin
-make  run-pandas run-polars run-dask run-pyspark
-#make  run-polars run-dask run-pandas run-modin run-duckdb run-pyspark
-#make plot
+comando=${comando:-"make run-all"}
 
-#echo run with IO
-#export RUN_INCLUDE_IO=1
-#make run-all
+#make tables SCALE_FACTOR=$SCALE_FACTOR
+
+eval "$comando"
+
 #make plot
